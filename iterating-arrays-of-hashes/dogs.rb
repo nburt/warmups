0b32862fec613a32846c38a405c722f0f58880f7
Nathanael Burt <nathanael.burt@gmail.com>
2014-04-30 09:31:19 -0600

    completed iterating arrays of hashes warmup

--- iterating-arrays-of-hashes/dogs.rb
+++ iterating-arrays-of-hashes/dogs.rb
@@ -1,11 +1,11 @@
 class Dogs
-  POOR      = (0..5).to_a.sample
-  AVERAGE   = (6..10).to_a.sample
+  POOR = (0..5).to_a.sample
+  AVERAGE = (6..10).to_a.sample
   EXCELLENT = (11..15).to_a.sample
 
   def initialize
-    joe   = {
-      :name => {:first => "Joe", :last=> "Smith"},
+    joe = {
+      :name => {:first => "Joe", :last => "Smith"},
       :owner_quality => EXCELLENT
     }
     sarah = {
@@ -29,30 +29,66 @@ class Dogs
   # only edit below this line
 
   def small_dogs
-    # only use the @dogs instance variable
+    small_dogs = []
+    @dogs.each do |dog|
+      if dog[:size].to_s == "small"
+        small_dogs << dog
+      end
+    end
+    small_dogs
   end
 
   def huge_dog
-    # only use the @dogs instance variable
+    @dogs.each do |dog|
+      if dog[:size].to_s == "huge"
+        return dog
+      end
+    end
   end
 
   def large_dog_names
-    # only use the @dogs instance variable
+    large_dog_names = []
+    @dogs.each do |dog|
+      if dog[:size].to_s == "large"
+        large_dog_names << dog[:name]
+      end
+    end
+    large_dog_names
   end
 
   def joes_large_dogs
-    # only use the @dogs instance variable
+    joes_large_dogs = []
+    @dogs.each do |dog|
+      if dog[:size].to_s == "large" && dog[:owner][:name][:first] == "Joe"
+        joes_large_dogs << dog[:name]
+      end
+    end
+    joes_large_dogs
   end
 
   def sizes
-    # only use the @dogs instance variable
+    sizes = []
+    @dogs.each do |dog|
+       sizes << dog[:size]
+    end
+    sizes.uniq
   end
 
   def owners
-    # only use the @dogs instance variable
+    owners = []
+    @dogs.each do |dog|
+      owners << "#{dog[:owner][:name][:first]} #{dog[:owner][:name][:last]}"
+    end
+    owners.uniq
   end
 
   def average_owners
-    # only use the @dogs instance variable and constants
+    average_owners = []
+    @dogs.each do |dog|
+      if dog[:owner][:owner_quality] == AVERAGE
+        average_owners << "#{dog[:owner][:name][:first]} #{dog[:owner][:name][:last]}"
+      end
+    end
+    average_owners.uniq
   end
 end
